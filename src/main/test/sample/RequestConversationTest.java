b713806836a3d7958346976a121d656778679fbb
mashafi3209 <54332351+mashafi3209@users.noreply.github.com>
2021-04-18 18:37:38 -0400

    Added more test to RequestConversationTest

--- src/main/test/sample/RequestConversationTest.java
+++ src/main/test/sample/RequestConversationTest.java
@@ -2,6 +2,7 @@ package sample;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,7 +11,7 @@ class RequestConversationTest {
   public RequestConversation requestConversation = new RequestConversation();
 
   @Test
-  void addButtonActionPerformed() throws SQLException {
+  void addButtonDoctor() throws SQLException {
     requestConversation.setRequestNumber(103);
     requestConversation.setUserID("JohnDoe");
     requestConversation.setUserType("Doctor");
@@ -19,16 +20,58 @@ class RequestConversationTest {
     Assertions.assertTrue(requestConversation.addButtonActionPerformed(null));
 
   }
+  @Test
+  void addButtonPatient() throws SQLException {
+    requestConversation.setRequestNumber(103);
+    requestConversation.setUserID("will");
+    requestConversation.setUserType("Patient");
+    requestConversation.setFinalString("I need to follow up soon");
+    requestConversation.setTestSignal(0);
+    Assertions.assertTrue(requestConversation.addButtonActionPerformed(null));
+
+  }
+
+
 
   @Test
-  void closeButtonActionPerformed() {
+  void closeButtonActionPerformedDoctor() {
+    requestConversation.setUserID("JohnDoe");
+    requestConversation.setUserType("Doctor");
+    requestConversation.setRequestNumber(103);
+    requestConversation.setTestSignal(0);
+    Assertions.assertTrue(requestConversation.closeButtonActionPerformed(null));
+
+
+  }
+  @Test
+  void closeButtonActionPerformedPatient() {
+    requestConversation.setUserID("will");
+    requestConversation.setUserType("Patient");
+    requestConversation.setRequestNumber(103);
+    requestConversation.setTestSignal(0);
+    Assertions.assertTrue(requestConversation.closeButtonActionPerformed(null));
+
+
   }
 
   @Test
-  void backButtonActionPerformed() {
+  void backButtonActionDoctor() {
+    requestConversation.setTestSignal(0);
+    requestConversation.setUserType("Doctor");
+    requestConversation.setUserID("JohnDoe");
+    Assertions.assertTrue(requestConversation.backButtonActionPerformed(null));
   }
-
   @Test
-  void main() {
+  void backButtonActionPatient() {
+    requestConversation.setTestSignal(0);
+    requestConversation.setUserType("Patient");
+    requestConversation.setUserID("will");
+    Assertions.assertTrue(requestConversation.backButtonActionPerformed(null));
+  }
+  @Test
+  public void testMain() throws IOException {
+    requestConversation.setUserID("will");
+    String[] args = null;
+    requestConversation.main(args);
   }
 }
